6c63e346b3746639244a16485dde96b8bd9aee83
uiryeol <soiris11@gmail.com>
2020-08-23 16:51:50 +0900

    test

---
+++ index.js
@@ -0,0 +1,13 @@
+document.querySelector('.red-header').addEventListener('click', function(e){
+  console.log(e.target);
+});
+
+document.querySelectorAll('.hoverable')
+  .forEach(function(hoverable) {
+    hoverable.addEventListener('mouseenter', function(){
+      hoverable.childNodes[3].classList.remove('hidden');
+    });
+    hoverable.addEventListener('mouseleave', function(){
+      hoverable.childNodes[3].classList.add('hidden');
+    });
+  })
